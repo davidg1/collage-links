c0aa3437856c23037477ab2837c26766f4b3b857
David Garrison <dcg484@gmail.com>
2024-03-06 19:26:09 -0500

    10th

--- scripts.js
+++ scripts.js
@@ -133,16 +133,900 @@ const vimeo4StarsData = [
 
 
 
+const vimeo1StarData = [
+    "https://vimeo.com/235081784",
+
+    "https://vimeo.com/381577571",
+    
+    "https://vimeo.com/306871386",
+    
+    "https://vimeo.com/316073024",
+    
+    "https://vimeo.com/318000989",
+    
+    "https://vimeo.com/318000754",
+    
+    "https://vimeo.com/711220656",
+    
+    "https://vimeo.com/225189795",
+    
+    "https://vimeo.com/luisbrock",
+    
+    "https://vimeo.com/357992757",
+    
+    "https://vimeo.com/330073128",
+    
+    "https://vimeo.com/296600806",
+    
+    "https://vimeo.com/450610512",
+    
+    "https://vimeo.com/644604788",
+    
+    "https://vimeo.com/722301214",
+    
+    "https://vimeo.com/352696938 ",
+    
+    "https://vimeo.com/394752343 ",
+    
+    "https://vimeo.com/495283549",
+    
+    "https://vimeo.com/350602960",
+    
+    "https://vimeo.com/445271341",
+    
+    "https://vimeo.com/801915366",
+    
+    "https://vimeo.com/445273112",
+    
+    "https://vimeo.com/267491776",
+    
+    "https://vimeo.com/445255542",
+    
+    "https://vimeo.com/730632074",
+    
+    "https://vimeo.com/51189923",
+    
+    "https://vimeo.com/628032119",
+    
+    "https://vimeo.com/495423038",
+    
+    "https://vimeo.com/439128440",
+    
+    "https://vimeo.com/165897314",
+    
+    "https://vimeo.com/447420446",
+    
+    "https://vimeo.com/574145771",
+    
+    "https://vimeo.com/502497208",
+    
+    "https://vimeo.com/786869291",
+    
+    "https://vimeo.com/457297433",
+    
+    "https://vimeo.com/211301747",
+    
+    "https://vimeo.com/444058924",
+    
+    "https://vimeo.com/444057814",
+    
+    "https://vimeo.com/247407036",
+    
+    "https://vimeo.com/10510603",
+    
+    "https://vimeo.com/8217610",
+    
+    "https://vimeo.com/858761409",
+    
+    "https://vimeo.com/214595828",
+    
+    "https://vimeo.com/717274812",
+    
+    "https://vimeo.com/520646809",
+    
+    "https://vimeo.com/153970605",
+    
+    "https://vimeo.com/574178450",
+    
+    "https://vimeo.com/520643830",
+    
+    "https://vimeo.com/509816298",
+    
+    "https://vimeo.com/694774343",
+    
+    "https://vimeo.com/832664023",
+    
+    "https://vimeo.com/727221952",
+    
+    "https://vimeo.com/809741451 ",
+    
+    "https://vimeo.com/814666289",
+    
+    "https://vimeo.com/789670262",
+    
+    "https://vimeo.com/562732136",
+    
+    "https://vimeo.com/211063667",
+    
+    "https://vimeo.com/556735725",
+    
+    "https://vimeo.com/65821003",
+    
+    "https://vimeo.com/3586032",
+    
+    "https://vimeo.com/159960592",
+    
+    "https://vimeo.com/159135145",
+    
+    "https://vimeo.com/42255974",
+    
+    "https://vimeo.com/572166647",
+    
+    "https://vimeo.com/42255973",
+    
+    "https://vimeo.com/74216981",
+    
+    "https://vimeo.com/693695632",
+    
+    "https://vimeo.com/39803147",
+    
+    "https://vimeo.com/457040654",
+    
+    "https://vimeo.com/617359946",
+    
+    "https://vimeo.com/407726749",
+    
+    "https://vimeo.com/470416515",
+    
+    "https://vimeo.com/412427838",
+    
+    "https://vimeo.com/334711856",
+    
+    "https://vimeo.com/307684781",
+    
+    "https://vimeo.com/683741344",
+    
+    "https://vimeo.com/135106112",
+    
+    "https://vimeo.com/443206347",
+    
+    "https://vimeo.com/675533857",
+    
+    "https://vimeo.com/159581216",
+    
+    "https://vimeo.com/222250786",
+    
+    "https://vimeo.com/325736107",
+    
+    "https://vimeo.com/251727502",
+    
+    "https://vimeo.com/34431454 ",
+    
+    "https://vimeo.com/569801124",
+    
+    "https://vimeo.com/486758210",
+    
+    "https://vimeo.com/482661437",
+    
+    "https://vimeo.com/482636113",
+    
+    "https://vimeo.com/475022198",
+    
+    "https://vimeo.com/457720828",
+    
+    "https://vimeo.com/820520059",
+    
+    "https://vimeo.com/819122757",
+    
+    "https://vimeo.com/680378297",
+    
+    "https://vimeo.com/339096856",
+    
+    "https://vimeo.com/264763456",
+    
+    "https://vimeo.com/74649184",
+    
+    "https://vimeo.com/549813303",
+    
+    "https://vimeo.com/185050725",
+    
+    "https://vimeo.com/471272688",
+    
+    "https://vimeo.com/435524222",
+    
+    "https://vimeo.com/632176402",
+    
+    "https://vimeo.com/162826505",
+    
+    "https://vimeo.com/125397383",
+    
+    "https://vimeo.com/70243706",
+    
+    "https://vimeo.com/781243191",
+    
+    "https://vimeo.com/655896263",
+    
+    "https://vimeo.com/441294251",
+    
+    "https://vimeo.com/453584003",
+    
+    "https://vimeo.com/439940734",
+    
+    "https://vimeo.com/479575582",
+    
+    "https://vimeo.com/761050772",
+    
+    "https://vimeo.com/107031194",
+    
+    "https://vimeo.com/124198365",
+    
+    "https://vimeo.com/327610617",
+    
+    "https://vimeo.com/327610592",
+    
+    "https://vimeo.com/640510030",
+    
+    "https://vimeo.com/865734434",
+    
+    "https://vimeo.com/504378302",
+    
+    "https://vimeo.com/4340911",
+    
+    "https://vimeo.com/283139663",
+    
+    "https://vimeo.com/289312645",
+    
+    "https://vimeo.com/373232153",
+    
+    "https://vimeo.com/820560079",
+    
+    "https://vimeo.com/680152528",
+    
+    "https://vimeo.com/484640725",
+    
+    "https://vimeo.com/365342053",
+    
+    "https://vimeo.com/758102356",
+    
+    "https://vimeo.com/98742998",
+    
+    "https://vimeo.com/470074359",
+    
+    "https://vimeo.com/156259253",
+    
+    "https://vimeo.com/2104440",
+    
+    "https://vimeo.com/269250156",
+    
+    "https://vimeo.com/394262801",
+    
+    "https://vimeo.com/24842092",
+    
+    "https://vimeo.com/315610394",
+    
+    "https://vimeo.com/165027052",
+    
+    "https://vimeo.com/86162901",
+    
+    "https://vimeo.com/356240431",
+    
+    "https://vimeo.com/435335196",
+    
+    "https://vimeo.com/798140496",
+    
+    "https://vimeo.com/680971526",
+    
+    "https://vimeo.com/411412397",
+    
+    "https://vimeo.com/832262048",
+    
+    "https://vimeo.com/772885248",
+    
+    "https://vimeo.com/563430433",
+    
+    "https://vimeo.com/413234788",
+    
+    "https://vimeo.com/150610453",
+    
+    "https://vimeo.com/588573534",
+    
+    "https://vimeo.com/496316182",
+    
+    "https://vimeo.com/496312501",
+    
+    "https://vimeo.com/194393994",
+    
+    "https://vimeo.com/547100622",
+    
+    "https://vimeo.com/538713770",
+    
+    "https://vimeo.com/122690200",
+    
+    "https://vimeo.com/393478170",
+    
+    "https://vimeo.com/554306994",
+    
+    "https://vimeo.com/466532291",
+    
+    "https://vimeo.com/437455075"
+
+];
+
+
+
+
+
+const vimeoNoStarsData = [
+
+
+    "https://vimeo.com/338065891",
+
+    "https://vimeo.com/410145083",
+    
+    "https://vimeo.com/410145655",
+    
+    "https://vimeo.com/435862113",
+    
+    "https://vimeo.com/437405385",
+    
+    "https://vimeo.com/438924819",
+    
+    "https://vimeo.com/456593381",
+    
+    "https://vimeo.com/461781311",
+    
+    "https://vimeo.com/466516081",
+    
+    "https://vimeo.com/466544366",
+    
+    "https://vimeo.com/467715143",
+    
+    "https://vimeo.com/522891192",
+    
+    "https://vimeo.com/565043915",
+    
+    "https://vimeo.com/561545619",
+    
+    "https://vimeo.com/270982916",
+    
+    "https://vimeo.com/807642749",
+    
+    "https://vimeo.com/254468215",
+    
+    "https://vimeo.com/236264611",
+    
+    "https://vimeo.com/207786092",
+    
+    "https://vimeo.com/484640725",
+    
+    "https://vimeo.com/410145655",
+    
+    "https://vimeo.com/360324407",
+    
+    "https://vimeo.com/218471266",
+    
+    "https://vimeo.com/236093051",
+    
+    "https://vimeo.com/347639779",
+    
+    "https://vimeo.com/37139830",
+    
+    "https://vimeo.com/112637450",
+    
+    "https://vimeo.com/128337996",
+    
+    "https://vimeo.com/174894131",
+    
+    "https://vimeo.com/135928103",
+    
+    "https://vimeo.com/462717854",
+    
+    "https://vimeo.com/750445351",
+    
+    "https://vimeo.com/750444151",
+    
+    "https://vimeo.com/194756366",
+    
+    "https://vimeo.com/224244339",
+    
+    "https://vimeo.com/357992622",
+    
+    "https://vimeo.com/534451810",
+    
+    "https://vimeo.com/232848931",
+    
+    "https://vimeo.com/154920777",
+    
+    "https://vimeo.com/105243276",
+    
+    "https://vimeo.com/113275114",
+    
+    "https://vimeo.com/163667628",
+    
+    "https://vimeo.com/496302178",
+    
+    "https://vimeo.com/513665406",
+    
+    "https://vimeo.com/684021813",
+    
+    "https://vimeo.com/635416728",
+    
+    "https://vimeo.com/700130536",
+    
+    "https://vimeo.com/59853964",
+    
+    "https://vimeo.com/513667791",
+    
+    "https://vimeo.com/95071749",
+    
+    "https://vimeo.com/509816298",
+    
+    "https://vimeo.com/148072574",
+    
+    "https://vimeo.com/651114274",
+    
+    "https://vimeo.com/862533906",
+    
+    "https://vimeo.com/420382065",
+    
+    "https://vimeo.com/469090266",
+    
+    "https://vimeo.com/398974111",
+    
+    "https://vimeo.com/243383608",
+    
+    "https://vimeo.com/745635080",
+    
+    "https://vimeo.com/714627510",
+    
+    "https://vimeo.com/503516956",
+    
+    "https://vimeo.com/484229525",
+    
+    "https://vimeo.com/379432450",
+    
+    "https://vimeo.com/578442274",
+    
+    "https://vimeo.com/400450676",
+    
+    "https://vimeo.com/428714527",
+    
+    "https://vimeo.com/321111253",
+    
+    "https://vimeo.com/561542761",
+    
+    "https://vimeo.com/573766112",
+    
+    "https://vimeo.com/562045651",
+    
+    "https://vimeo.com/713715998",
+    
+    "https://vimeo.com/705905341",
+    
+    "https://vimeo.com/359111873",
+    
+    "https://vimeo.com/400450676",
+    
+    "https://vimeo.com/361688831",
+    
+    "https://vimeo.com/436482189",
+    
+    "https://vimeo.com/297843603",
+    
+    "https://vimeo.com/187834150",
+    
+    "https://vimeo.com/253892735",
+    
+    "https://vimeo.com/351638053",
+    
+    "https://vimeo.com/232478613",
+    
+    "https://vimeo.com/185486940",
+    
+    "https://vimeo.com/650953579",
+    
+    "https://vimeo.com/16230434",
+    
+    "https://vimeo.com/246302219",
+    
+    "https://vimeo.com/53983028",
+    
+    "https://vimeo.com/574144785",
+    
+    "https://vimeo.com/392348175",
+    
+    "https://vimeo.com/416870476",
+    
+    "https://vimeo.com/415311730",
+    
+    "https://vimeo.com/815700582",
+    
+    "https://vimeo.com/445268288",
+    
+    "https://vimeo.com/412092721",
+    
+    "https://vimeo.com/253212180",
+    
+    "https://vimeo.com/495832167",
+    
+    "https://vimeo.com/445257151",
+    
+    "https://vimeo.com/645363983",
+    
+    "https://vimeo.com/19471332",
+    
+    "https://vimeo.com/445259919",
+    
+    "https://vimeo.com/386706285",
+    
+    "https://vimeo.com/674122989",
+    
+    "https://vimeo.com/181579636",
+    
+    "https://vimeo.com/674119433",
+    
+    "https://vimeo.com/674122819",
+    
+    "https://vimeo.com/20515184",
+    
+    "https://vimeo.com/208610378",
+    
+    "https://vimeo.com/443860107",
+    
+    "https://vimeo.com/772847061",
+    
+    "https://vimeo.com/674120572",
+    
+    "https://vimeo.com/829599902",
+    
+    "https://vimeo.com/34156354",
+    
+    "https://vimeo.com/645372054",
+    
+    "https://vimeo.com/629539126",
+    
+    "https://vimeo.com/816022349",
+    
+    "https://vimeo.com/443861502",
+    
+    "https://vimeo.com/357704009",
+    
+    "https://vimeo.com/427069108",
+    
+    "https://vimeo.com/801094585",
+    
+    "https://vimeo.com/351124433",
+    
+    "https://vimeo.com/498647606",
+    
+    "https://vimeo.com/36480169",
+    
+    "https://vimeo.com/537904949",
+    
+    "https://vimeo.com/574144834",
+    
+    "https://vimeo.com/20920042",
+    
+    "https://vimeo.com/51095881",
+    
+    "https://vimeo.com/153471641",
+    
+    "https://vimeo.com/267492403",
+    
+    "https://vimeo.com/688199039",
+    
+    "https://vimeo.com/443858136",
+    
+    "https://vimeo.com/517770137",
+    
+    "https://vimeo.com/443859422",
+    
+    "https://vimeo.com/444057283",
+    
+    "https://vimeo.com/185814005",
+    
+    "https://vimeo.com/273359417",
+    
+    "https://vimeo.com/861092417",
+    
+    "https://vimeo.com/762711813",
+    
+    "https://vimeo.com/412729159",
+    
+    "https://vimeo.com/792297880",
+    
+    "https://vimeo.com/414695857",
+    
+    "https://vimeo.com/425114394",
+    
+    "https://vimeo.com/531868696",
+    
+    "https://vimeo.com/271959584",
+    
+    "https://vimeo.com/138665550",
+    
+    "https://vimeo.com/743940490",
+    
+    "https://vimeo.com/799617341",
+    
+    "https://vimeo.com/357034347",
+    
+    "https://vimeo.com/800085685",
+    
+    "https://vimeo.com/786317724",
+    
+    "https://vimeo.com/800695445",
+    
+    "https://vimeo.com/458126844",
+    
+    "https://vimeo.com/428855560",
+    
+    "https://vimeo.com/510213435",
+    
+    "https://vimeo.com/665659602",
+    
+    "https://vimeo.com/689968731",
+    
+    "https://vimeo.com/666794928",
+    
+    "https://vimeo.com/730555460",
+    
+    "https://vimeo.com/42255975",
+    
+    "https://vimeo.com/689968935",
+    
+    "https://vimeo.com/457040595",
+    
+    "https://vimeo.com/457040727",
+    
+    "https://vimeo.com/331680511",
+    
+    "https://vimeo.com/260971083",
+    
+    "https://vimeo.com/575514159",
+    
+    "https://vimeo.com/267509669",
+    
+    "https://vimeo.com/384242462",
+    
+    "https://vimeo.com/142603290",
+    
+    "https://vimeo.com/208155941",
+    
+    "https://vimeo.com/305409608",
+    
+    "https://vimeo.com/754925363",
+    
+    "https://vimeo.com/308862852",
+    
+    "https://vimeo.com/320344703",
+    
+    "https://vimeo.com/699044253",
+    
+    "https://vimeo.com/369487494",
+    
+    "https://vimeo.com/403494609",
+    
+    "https://vimeo.com/751234259",
+    
+    "https://vimeo.com/339148070",
+    
+    "https://vimeo.com/311221689",
+    
+    "https://vimeo.com/266722764",
+    
+    "https://vimeo.com/50832691",
+    
+    "https://vimeo.com/792569185",
+    
+    "https://vimeo.com/94781398",
+    
+    "https://vimeo.com/707722858",
+    
+    "https://vimeo.com/141656332",
+    
+    "https://vimeo.com/290105400",
+    
+    "https://vimeo.com/730486805",
+    
+    "https://vimeo.com/150864781",
+    
+    "https://vimeo.com/515358429",
+    
+    "https://vimeo.com/394555371",
+    
+    "https://vimeo.com/10164974",
+    
+    "https://vimeo.com/580895357",
+    
+    "https://vimeo.com/328987901",
+    
+    "https://vimeo.com/163725539",
+    
+    "https://vimeo.com/256005863",
+    
+    "https://vimeo.com/49535151",
+    
+    "https://vimeo.com/538781553",
+    
+    "https://vimeo.com/243519677",
+    
+    "https://vimeo.com/727896077",
+    
+    "https://vimeo.com/727118952",
+    
+    "https://vimeo.com/331680769",
+    
+    "https://vimeo.com/220595568",
+    
+    "https://vimeo.com/379916949",
+    
+    "https://vimeo.com/381188833",
+    
+    "https://vimeo.com/96023680",
+    
+    "https://vimeo.com/271897818",
+    
+    "https://vimeo.com/34668853",
+    
+    "https://vimeo.com/96862926",
+    
+    "https://vimeo.com/268414942",
+    
+    "https://vimeo.com/40167848",
+    
+    "https://vimeo.com/547858326",
+    
+    "https://vimeo.com/54995144",
+    
+    "https://vimeo.com/379572468",
+    
+    "https://vimeo.com/102190336",
+    
+    "https://vimeo.com/700452312",
+    
+    "https://vimeo.com/667724272",
+    
+    "https://vimeo.com/846647691",
+    
+    "https://vimeo.com/60898178",
+    
+    "https://vimeo.com/124107005",
+    
+    "https://vimeo.com/753140172",
+    
+    "https://vimeo.com/777876112",
+    
+    "https://vimeo.com/651702695",
+    
+    "https://vimeo.com/208413381",
+
+];
+
+
+
+const bilData = [
+    "https://vimeo.com/123139314",
+
+    "https://vimeo.com/122774160",
+    
+    "https://vimeo.com/73489162",
+    
+    "https://vimeo.com/69999411",
+    
+    "https://vimeo.com/69999296",
+    
+    "https://vimeo.com/218852440",
+    
+    "https://vimeo.com/11251739",
+    
+    "https://vimeo.com/218852479",
+    
+    "https://vimeo.com/123364677",
+    
+    "https://vimeo.com/58226102",
+    
+    "https://vimeo.com/40322147",
+    
+    "https://vimeo.com/44675151",
+    
+    "https://vimeo.com/40111967",
+    
+    "https://vimeo.com/17312647",
+    
+    "https://vimeo.com/17312637",
+    
+    "https://vimeo.com/5794371",
+    
+    "https://vimeo.com/5647489",
+    
+    "https://vimeo.com/112453419"
+
+];
+
+
+
+const gifsData = [
+    "https://scorpiondagger.tumblr.com/",
+
+    "https://www.pinterest.com/pin/156077943322854720/",
+    
+    "https://www.pinterest.com/pin/526991593909837965/",
+    
+    "https://www.pinterest.com/pin/488781365802290336/",
+    
+    "https://www.pinterest.com/pin/359584351511459238/",
+    
+    "https://www.pinterest.com/pin/691021136589048584/",
+    
+    "https://www.pinterest.com/pin/179369997638730836/",
+    
+    "https://www.pinterest.com/pin/342906959145269172/",
+    
+    "https://www.pinterest.com/pin/789044797228818481/",
+    
+    "https://www.pinterest.com/pin/68742390092/",
+    
+    "https://www.boredpanda.com/classic-painting-gifs-kiszkiloszki/",
+    
+    "https://www.pinterest.com/pin/572801646365513012/",
+    
+    "https://www.pinterest.com/pin/360710251408674258/"
+];
+
+
+
+const dailyMotionData = [
+    "https://www.dailymotion.com/video/x363mdp",
+    "https://www.dailymotion.com/video/x31vfxa",
+    "https://www.dailymotion.com/video/x52xim",
+    "https://www.dailymotion.com/video/x8ferg",
+    "https://www.dailymotion.com/video/x4oozj"
+];
+
+
+
+const youtubeData = [
+    "https://youtu.be/9RAt5va9Tmk?feature=shared",
+    "https://youtube.com/watch?v=pKX9Oi62nEg&feature=shared"
+];
+
+
+
+
+
+/***************************************************************************************************************/
+/***************************************************************************************************************/
+/***************************************************************************************************************/
+/***************************************************************************************************************/
+
+
     
 const vimeo4StarsList = document.getElementById("vimeo-4-stars-list");
-createList(vimeo4StarsList, vimeo4StarsData);
+createList(vimeo4StarsList, vimeo4StarsData, "V4S");
+
+const bilList = document.getElementById("bil-list");
+createList(bilList, bilData, "BIL");
+
+const dailyMotionList = document.getElementById("daily-motion-list");
+createList(dailyMotionList, dailyMotionData, "DM");
+
+const gifsList = document.getElementById("gifs-list");
+createList(gifsList, gifsData, "GIFs");
+
+const youtubeList = document.getElementById("youtube-list");
+createList(youtubeList, youtubeData, "YT");
+
+const vimeo1StarList = document.getElementById("vimeo-1-star-list");
+createList(vimeo1StarList, vimeo1StarData, "V1S");
+
+const vimeoNoStarsList = document.getElementById("vimeo-no-stars-list");
+createList(vimeoNoStarsList, vimeoNoStarsData, "VNS");
 
 
 
 
-
-
-function createList(list, arr) {
+function createList(list, arr, prefix) {
 
     for (i = 0; i < arr.length; i++) {
         let li = document.createElement('li');
@@ -152,7 +1036,7 @@ function createList(list, arr) {
         a.setAttribute("target", "_blank");  
         a.setAttribute("href", arr[i]);  
     
-        a.innerText = "Link " + i;
+        a.innerText = prefix + " Link " + i;
         
         li.appendChild(a);
         
